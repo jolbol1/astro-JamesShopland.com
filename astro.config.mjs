3199994223ee0e110528ec25565ae8ad55166429
jolbol1 <github@jamesshopland.com>
2025-01-03 05:46:07 +0000

    fix file path

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,8 +12,8 @@ import expressiveCode from "astro-expressive-code"
 import { rehypeAccessibleEmojis } from "rehype-accessible-emojis"
 import rehypeAutolinkHeadings from "rehype-autolink-headings"
 import rehypeSlug from "rehype-slug"
-import { CopyFilesPlugin } from "sitemap-copy"
 
+import { CopyFilesPlugin } from "./sitemap-copy"
 import { autolinkConfig } from "./src/lib/headings"
 import { remarkOembedOptions } from "./src/lib/oembed"
 
